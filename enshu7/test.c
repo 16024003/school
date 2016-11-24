2a9106f02e68ef6dd092de7d575b6501c010b2d2
j16024003 <j16024003@ubuntu.(none)>
2016-11-24 16:04:34 +0900

    enshu7

--- enshu7/test.c
+++ enshu7/test.c
@@ -1,1 +1,11 @@
-#include <stdio.h>
+for(i=0;i<10;i++)
+{
+    if(str1[i]=='\0') break;
+    else
+    {
+        if((str1[i]>='a')&&(str1[i]<='z'))
+        {
+            str1[i]-=32;
+        }
+    }
+}
