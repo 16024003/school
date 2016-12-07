8c6322f4a274bb527eecb4f520b08543dc2998f2
j16024003 <j16024003@ubuntu.(none)>
2016-12-07 11:41:38 +0900

    なんでもいいよ

---
+++ enshu8/e63.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+void swap(int *x, int *y);
+
+main (void){
+    int n,i,j,flag=1;
+    printf("入力する数字の個数を入力してください");
+    scanf("%d",&n);
+    int m[n];
+    printf("数字を入力してください");
+    for(i=0;i<n;i++){
+        scanf("%d",&m[i]);
+    }
+
+    while(flag>0){
+        flag=0;
+        for(i=0;i<n-1;i++){
+            if(m[i]>m[i+1]){
+                swap(&m[i],&m[i+1]);
+                flag++;
+            }
+        }
+    }
+    printf("小さい順に並べると ");
+    for(i=0;i<n;i++){
+        printf("%d ",m[i]);
+    }
+    printf("です\n");
+
+    return 0;
+}
+
+void swap(int *x, int *y){
+    int temp;
+    
+    temp=*x;
+    *x=*y;
+    *y=temp;
+
+}
