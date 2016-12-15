7bef13c2350b052cacddfd1c37e05ed76cf1fee6
Kotaro Akahori <kota2272@me.com>
2016-12-15 12:49:19 +0900

    enshu9

--- enshu9/renshu4-1.c
+++ enshu9/renshu4-1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-int product(int n,int m);
+int Delta(int a,int b,int c,int d);
 
 int main(void)
 {
-    int f1[3],f2[3],i,delta;
+    int f1[3],f2[3],i;
     double x,y;
 
     printf("2つの方程式を入力してください\nax+by=h ");
@@ -17,26 +17,24 @@ int main(void)
         scanf("%d",&f2[i]);
     }
 
-    delta=product(f1[0],f2[1])-product(f1[1],f2[0]);
-
-    if(delta==0)
+    if(Delta(f1[0],f1[1],f2[0],f2[1])==0)
     {
         printf("ERROR!ad-bc=0になる連立方程式です\n");
     }
     else 
     {
-        x=(product(f1[2],f2[1])-product(f1[1],f2[2]))/delta;
-        y=(product(f1[0],f2[2])-product(f1[2],f2[0]))/delta;
+        x=(f1[2]*f2[1]-f1[1]*f2[2])/Delta(f1[0],f1[1],f2[0],f2[1]);
+        y=(f1[0]*f2[2]-f1[2]*f2[0])/Delta(f1[0],f1[1],f2[0],f2[1]);
         printf("xの値は%f,yの値は%fです。\n",x,y);
     }
     return 0;
 }
 
-
-int product(int n,int m)
+int Delta(int a,int b,int c,int d)
 {
-    int h;
+    int delta;
 
-    h=n*m;
-    return h;
+    delta=a*d-b*c;
+    return delta;
 }
+
--- enshu9/renshu4-2.c
+++ enshu9/renshu4-2.c
@@ -3,21 +3,21 @@ int product(int n,int m);
 
 int main(void)
 {
-    int f1[3],f2[3],i,delta;
+    int *f1[3],*f2[3],i,delta;
     double x,y;
 
     printf("2つの方程式を入力してください\nax+by=h ");
     for(i=0;i<3;i++)
     {
-        scanf("%d",&f1[i]);
+        scanf("%d",f1[i]);
     }
     printf("cx+dy=k ");
         for(i=0;i<3;i++)
     {
-        scanf("%d",&f2[i]);
+        scanf("%d",f2[i]);
     }
 
-    delta=product(f1[0],f2[1])-product(f1[1],f2[0]);
+    delta=f1[0]*f2[1]-f1[1]*f2[0];
 
     if(delta==0)
     {
@@ -25,18 +25,11 @@ int main(void)
     }
     else 
     {
-        x=(product(f1[2],f2[1])-product(f1[1],f2[2]))/delta;
-        y=(product(f1[0],f2[2])-product(f1[2],f2[0]))/delta;
+        x=(f1[2]*f2[1]-f1[1]*f2[2])/delta;
+        y=(f1[0]*f2[2]-f1[2]*f2[0])/delta;
         printf("xの値は%f,yの値は%fです。\n",x,y);
     }
     return 0;
 }
 
-
-int product(int n,int m)
-{
-    int h;
-
-    h=n*m;
-    return h;
-}
+int Delta
